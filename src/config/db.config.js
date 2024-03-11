ae68bbf9ca7cbd433c6266c7108936438a419947
lfzapata791@soy.sena.edu.co <lfzapata791@soy.sena.edu.co>
2024-03-11 10:54:38 -0500

    Primeros avances video 1

---
+++ src/config/db.config.js
@@ -0,0 +1,10 @@
+
+//config/db.config.js
+//conexion a base de datos mysql
+export const dbconfig = {
+    host:'localhost',
+    user:'root',
+    password:'ROOT',
+    database:'crudProductos',
+    port:3306 //si or alguna circunstancia se conecta a un puerto  distinto de defecto port 3308
+}
---
+++ src/model/productModel.js
@@ -0,0 +1,34 @@
+//models/productModel.js
+//creacion modelo producto
+
+import {dbconfig} from '../config/db.config.js';
+import mysql from 'mysql2';
+
+const db = mysql.createPool(dbconfig);
+
+//creacion obtener todos los productos
+
+// getAll
+export const getProducts = async() => {
+    const [rows ] = await pool.query('SElECT * from products');
+    return rows;
+};
+
+//obtener producto por id
+//getById
+export const getProductById = async (productId) =>{
+    const [rows ] = await pool.query('SElECT * from products WHERE id=?',productId);
+    return rows[0];
+}
+
+//agregar nuevo producto
+export const createProduct = async (productData) =>{
+    const {name,price,description} = productData;
+    const [result] = await pool.query('INSERT INTO products(name,price,description) VALUES(?,?,?)',[name,price,description]);
+    return result.insertId;
+}
+//actualizar producto existente
+export const updateProduct = async(productId,productData)=>{
+    const {name,price,description} = productData;
+    const [result] = await pool.query('UPDATE products SET name=?,price=?,description=? WHERE id=?',[name,price,description,productId]);
+}
